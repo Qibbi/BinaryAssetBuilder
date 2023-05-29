7ebe8b8a65660503b5aecfdfbc2306d49f4d448b
Jana Mohn <Ingoneur@gmx.de>
2023-05-29 21:44:50 +0200

    Fixup

--- source/BinaryAssetBuilder.XmlCompiler/Marshaler.Modules.MetaGameLifetimeUpdate.cs
+++ source/BinaryAssetBuilder.XmlCompiler/Marshaler.Modules.MetaGameLifetimeUpdate.cs
@@ -4,13 +4,13 @@ using SageBinaryData;
 
 public static partial class Marshaler
 {
-    public static unsafe void Marshal(Node node, MetaGameLifetimeUpdate* objT, Tracker state)
+    public static unsafe void Marshal(Node node, MetaGameLifetimeUpdateModuleData* objT, Tracker state)
     {
         if (node is null)
         {
             return;
         }
-        Marshal(node.GetAttributeValue(nameof(MetaGameLifetimeUpdate.LifeTimeTurns), "1"), &objT->LifeTimeTurns, state);
+        Marshal(node.GetAttributeValue(nameof(MetaGameLifetimeUpdateModuleData.LifeTimeTurns), "1"), &objT->LifeTimeTurns, state);
         Marshal(node, (UpdateModuleData*)objT, state);
     }
 }
